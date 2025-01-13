ba39cc2ec90a4fe8afa0ec7783f140a143eb5eeb
Miguel Pereira <miguel.pereira@closer.pt>
2025-01-13 11:28:49 +0000

    Updated config.ini and schedule.yml files

--- scheduler_update.py
+++ scheduler_update.py
@@ -25,9 +25,9 @@ def main():
     #####
     for section in config.sections():
         print(f"[{section}]")
-    for option in config.options(section):
-        value = config.get(section, option)
-        print(f"{option} = {value}")
+        for option in config.options(section):
+            value = config.get(section, option)
+            print(f"{option} = {value}")
     #####
     classes_dict = eval(config['Classes to Schedule by Day']['classes_dict'])
     classes_dict = {key: value for key, value in classes_dict.items() if value} # remove empty days
